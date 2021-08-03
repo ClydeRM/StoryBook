2141a4cbf4d66045b2e460fa1967fcecea93b7f9
clyde <40743130@gm.nfu.edu.tw>
2021-08-03 19:04:04 +0800

    body parser session and stories route

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const express = require('express');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
 const passport = require('passport');
 const session = require('express-session');
+const MongoStore = require('connect-mongo');
 const connectDB = require('./config/db');
 
 // Load config
@@ -19,6 +21,10 @@ connectDB();
 // Create app
 const app = express();
 
+// Body parser
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 // Logging
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
@@ -29,11 +35,14 @@ app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', '.hbs');
 
 // Sessions
-app.use(session({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: false,
-}))
+app.use(
+    session({
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: false,
+        store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
+    })
+)
 
 //  Passport middleware
 app.use(passport.initialize());
@@ -45,11 +54,13 @@ app.use(express.static(path.join(__dirname, 'public')))
 // Routes
 app.use('/', require('./routes/index'));
 app.use('/auth', require('./routes/auth'));
+app.use('/stories', require('./routes/stories'));
+
 
 // Listening PORT
 const PORT = process.env.PORT || 3000;
 
 app.listen(
     PORT,
-    console.log(`Server runnning in ${process.env.NODE_ENV} mode on http://localhost:${PORT}`)
+    console.log(`Server running in ${process.env.NODE_ENV} mode on http://localhost:${PORT}`)
 );
